ef77581a1022cd0ebf2bc89c3f01061f6a165f75
Torjer <andres22jld@gmail.com>
2021-06-01 21:56:47 -0600

    Added cluster map
    Added cluster map in the index page, with links to each individual campground

--- seeds/index.js
+++ seeds/index.js
@@ -19,13 +19,16 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 1
         const camp = new Campground({
             geometry: {
                 type: "Point",
-                coordinates: [-113, 47]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
             },
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             image: [
